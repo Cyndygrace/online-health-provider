1794a9233214905edaa71d53ef968b5370ed82f7
Cynthia Iloekwe <cynthiailoekwe@gmail.com>
2019-08-01 10:36:09 +0100

    setup graphqlHTTP on a single route to an end point

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
-
 const app = express();
+
+
+// we set up an express server that runs graphql api
+const graphqlHTTP = require('express-graphql')
+
+// this is done by using graphqlHTTP as a middleware on a single route which will serve as an end point to communicate with our graphql data.
+app.use('/graphql', graphqlHTTP({
+// graphqlHTTP must contain a schema of how the graph of our data looks(structure, datatype and relationship.)
+}))
+
+
+
+
 
 app.listen(4000, () => {
   console.log('now listening on port 4000...')
 })
+// setup graphql to work with our express application. installed graphql & express-graphql
